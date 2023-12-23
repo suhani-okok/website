f8df4ebcaa28e75d315751c3ed135e071142d786
Suhani MIshra <suhani.okok@gmail.com>
2023-12-24 05:10:30 +0530

    changes made

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-// JavaScript functionality
+
 document.getElementById('contactForm').addEventListener('submit', function(event) {
-  event.preventDefault(); // Prevent the default form submission
-  // You can add your logic here to handle form submission (e.g., sending data to a server)
-  // For this example, let's just log the form data to the console
+  event.preventDefault(); 
+  
+  
   const formData = new FormData(event.target);
   for (let [name, value] of formData.entries()) {
     console.log(`${name}: ${value}`);
